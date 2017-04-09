f661454ab404e87da546676180d4d0c34d1397f2
bhutchin <brent.hutchin>
2017-04-09 13:02:09 +1000

    updated quoting

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -12,15 +12,15 @@ module.exports = function (robot) {
           msg2.reply("Sure what was the subject?");
           dialog.addChoice(/(.*)/i, function (msg3) {
               var subject = msg3.match[0];
-              subject = "\'"+subject+"\'";
+              subject = "\""+subject+"\"";
               msg3.reply("And what was the rest of the info?");
                 dialog.addChoice(/(.*)/i, function (msg4) {
                 var body = msg4.match[0]
                 body = "\""+body+"\"";
                 msg4.reply("Forwarding that on now.");
                   var child_process = require('child_process');
-                  var sender = msg.message.user.name.toLowerCase()
-                  sender = sender + " ";
+                  //var sender = msg.message.user.name.toLowerCase()
+                  var sender = "\"brent.hutchin\"";
                   child_process.exec('/usr/local/bin/createticket.sh ' + sender + subject + body,
                   function (error, stdout, stderr) {
                     if (error !== null) {
