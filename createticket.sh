c3f9ea9c7aed2f647c20007d7726f8190f6cf288
bhutchin <brent.hutchin@gmail.com>
2017-04-10 19:38:42 +1000

    update createticket updated formatting

--- createticket.sh
+++ createticket.sh
@@ -1,7 +1,19 @@
 #! /bin/sh
-echo "" >> /tmp/rocket.log
-echo "" >> /tmp/rocket.log
-echo "Val 1" $1 >> /tmp/rocket.log
-echo "Val 2" $2 >> /tmp/rocket.log
-echo "Val 3" $3 >> /tmp/rocket.log
+ARGS="brent.hutchin@@rockethal subject testing%%rockethal blah blah body blah"
+USERNAME=$($ARGS | sed s%\@\@.*%%g)
+SUBJECT=$(${ARGS} | sed s%.*\@\@%%g | sed s/\%\%.*//g)
+MESSAGE=$3
+#echo ""  >> /var/log/rockethal.log
+#echo "Email args:"  >> /var/log/rockethal.log
+#echo "Username:" $USERNAME >> /var/log/rockethal.log
+echo $ARGS #>> /var/log/rockethal.log
+echo "Username:"$USERNAME
+echo "Subject:" $SUBJECT
+echo "Body:"    $BODY
+
+
+#SEND="/bin/mail -r $USERNAME -s $SUBJECT"
+#TO='brent.hutchin@iloura.com.au'
+#echo $MESSAGE | $SEND $TO
+
 echo "Val all" $@ >> /tmp/rocket.log
